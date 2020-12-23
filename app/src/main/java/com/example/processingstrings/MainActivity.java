c95354e52388b458afffcabca66c2b134518aa7c
livantius <76242373+livantius@users.noreply.github.com>
2020-12-24 02:00:13 +0700

    4. Добавление кнопки Gen, которая будет выводить новую фразу.

--- app/src/main/java/com/example/processingstrings/MainActivity.java
+++ app/src/main/java/com/example/processingstrings/MainActivity.java
@@ -10,17 +10,24 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv_phrase;
-
+    private Button bu_gen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv_phrase = findViewById(R.id.tv_phrase);
 
+        tv_phrase = findViewById(R.id.tv_phrase);
+        bu_gen = findViewById(R.id.bu_gen);
 
         tv_phrase.setText("Всё, что нам нужно – это " + (Phraser.PhraserGen()));
+        bu_gen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tv_phrase.setText("Всё, что нам нужно – это " + (Phraser.PhraserGen()));
+            }
+        });
 
     }
-
 }
