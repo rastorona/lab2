a37b731e3590100e316118200b969e5f9e4b3c1f
livantius <76242373+livantius@users.noreply.github.com>
2020-12-24 01:34:34 +0700

    3. добавление в MainActivity вызова метода PhraserGen, с добавлением к тексту «Всё, что нам нужно – это»    сгенерированную фразу.

--- app/src/main/java/com/example/processingstrings/MainActivity.java
+++ app/src/main/java/com/example/processingstrings/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.processingstrings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_phrase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_phrase = findViewById(R.id.tv_phrase);
+
+
+        tv_phrase.setText("Всё, что нам нужно – это " + (Phraser.PhraserGen()));
+
     }
+
 }
