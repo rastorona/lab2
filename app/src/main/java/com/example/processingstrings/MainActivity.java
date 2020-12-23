e860c688119991b9da8e6b6d8dd5584df0897e03
livantius <76242373+livantius@users.noreply.github.com>
2020-12-24 02:10:20 +0700

    6. Дополнение кода приложения таким образом, чтобы поменять местами подстроки до и после фразы    « – это». Результат вставьте в текстовое поле tv_result.

--- app/src/main/java/com/example/processingstrings/MainActivity.java
+++ app/src/main/java/com/example/processingstrings/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView tv_phrase;
     private Button bu_gen;
+    private TextView tv_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +20,24 @@ public class MainActivity extends AppCompatActivity {
 
         tv_phrase = findViewById(R.id.tv_phrase);
         bu_gen = findViewById(R.id.bu_gen);
+        tv_result = findViewById(R.id.tv_result);
 
         tv_phrase.setText("Всё, что нам нужно – это " + (Phraser.PhraserGen()));
+        tv_result.setText(reverse());
         bu_gen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 tv_phrase.setText("Всё, что нам нужно – это " + (Phraser.PhraserGen()));
+                tv_result.setText(reverse());
             }
         });
 
+    }
+    public String reverse () {
+        String s = tv_phrase.getText().toString();
+        String left = s.substring(0,s.indexOf("– это")-1);
+        String right = s.substring(s.indexOf("– это")+7);
+        return  Character.toUpperCase(s.charAt(s.indexOf("– это")+6))  + (right+" - это "+left+".").toLowerCase();
     }
 }
